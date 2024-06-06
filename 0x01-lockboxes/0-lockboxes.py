3e469017921b740bf551a8102180d99707698b5e
root <Gideon5348>
2024-06-06 00:31:21 -0700

    0x01. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+"""
+Module for checking if all boxes can be unlocked.
+"""
+
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Parameters:
+    boxes (list of list of int): List of lists where each sublist contains keys
+    to other boxes.
+
+    Returns:
+    bool: True if all boxes can be unlocked, False otherwise.
+    """
     n = len(boxes)
     visited = [False] * n
     stack = [0]
     visited[0] = True
     opened_boxes = 1
-    
+
     while stack:
         current_box = stack.pop()
         for key in boxes[current_box]:
@@ -13,5 +28,5 @@ def canUnlockAll(boxes):
                 visited[key] = True
                 stack.append(key)
                 opened_boxes += 1
-                
+
     return opened_boxes == n
