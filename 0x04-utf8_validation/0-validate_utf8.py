8546ae5bd442609bf0c24ef8e7616ea2acc8a3c1
Gideon5348 <gideonmutai096@gmail.com>
2024-06-29 03:54:29 -0700

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,42 +1,52 @@
 #!/usr/bin/python3
 """
-A method that determines if a given data set reps a valid UTF-8 encoding."""
+A method that determines if a given data set represents a valid UTF-8 encoding.
+"""
 
 
 def validUTF8(data):
     """
-    Determine if a given data set represents a valid UTF-8 encoding.
+    Validate if a given data set represents a valid UTF-8 encoding.
 
-    Args:
-    data : List[int]
-        A list of integers representing bytes of data.
+    Parameters:
+    data (List[int]): A list of integers representing bytes of data
 
     Returns:
-    bool
-        True if data is a valid UTF-8 encoding, else False.
+    bool: True if data is a valid UTF-8 encoding, else False
     """
+
+    # Number of bytes in the current UTF-8 character
     num_bytes = 0
 
-    for byte in data:
-        if byte > 255:
-            return False
+    # Masks to check the significant bits in a byte
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
 
-        binary_repr = format(byte, '08b')
+    for byte in data:
+        # Get only the least significant 8 bits of the integer
+        byte = byte & 0xFF
 
         if num_bytes == 0:
-            if binary_repr[0] == '0':
+            # Check how many 1s are at the start of the byte
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask = mask >> 1
+
+            # 1 byte characters
+            if num_bytes == 0:
                 continue
-            elif binary_repr[:3] == '110':
-                num_bytes = 1
-            elif binary_repr[:4] == '1110':
-                num_bytes = 2
-            elif binary_repr[:5] == '11110':
-                num_bytes = 3
-            else:
+
+            # If the number of bytes is more than 4 or less than 2
+            if num_bytes == 1 or num_bytes > 4:
                 return False
+
         else:
-            if binary_repr[:2] != '10':
+            # If this byte is not starting with 10, return False
+            if not (byte & mask1 and not (byte & mask2)):
                 return False
-            num_bytes -= 1
+
+        # We processed one byte
+        num_bytes -= 1
 
     return num_bytes == 0
