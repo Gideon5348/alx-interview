d0d71d65fb9e8b4483b52a44963987717f941948
Gideon5348 <gideonmutai096@gmail.com>
2024-07-11 10:23:17 -0700

    0x06. Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get the Movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Define the URL for the Star Wars API
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+// Make a request to the Star Wars API
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  // Parse the response body as JSON
+  const film = JSON.parse(body);
+
+  // Get the list of character URLs
+  const characters = film.characters;
+
+  // For each character URL, make a request to get the character name
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error('Error:', charError);
+        return;
+      }
+
+      // Parse the character response body as JSON
+      const character = JSON.parse(charBody);
+
+      // Print the character name
+      console.log(character.name);
+    });
+  });
+});
